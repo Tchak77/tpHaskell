fe5bdde2ff012d47fd71dfb4b05bcbdd4ff2ed9a
Derfic <Derfic77@gmail.com>
2017-02-04 18:10:28 +0100

    Finito

--- haskellproj.hsproj/Haskellproj.hs
+++ haskellproj.hsproj/Haskellproj.hs
@@ -10,7 +10,7 @@ permutations xs0 =  xs0 : perms xs0 []
             interleave' _ [] r = (ts, r)
             interleave' f (y:ys) r = let (us,zs) = interleave' (f . (y:)) ys r
                                      in  (y:us, f (t:y:us) : zs)
------------------------------------------------
+---------------------------------------------------
 data Op = Add | Sub | Mul | Div
 data Expr = Val Int | App Op Expr Expr 
 
@@ -33,7 +33,8 @@ valid Div x y = mod x y == 0 -- integer division only
 values :: Expr -> [Int]
 values (Val n) = [n]
 values (App _ l r) = values l ++ values r
---------
+---------------------------------------------------
+-- Returns the result of an expression
 eval :: Expr -> Int
 eval (Val n) = n 
 eval (App Add x y) = eval x + eval y
@@ -81,7 +82,7 @@ filterValid = filter isValidExpr
 getFirstExprEvaluated :: [Expr] -> Int -> Maybe Expr
 getFirstExprEvaluated [] _ = Nothing
 getFirstExprEvaluated (x:xs) n 
-                           | eval x == n = Just x -- Here x MUST be evaluable (with filter) !
+                           | eval x == n = Just x -- Here x MUST be evaluable (filtered) !
                            | otherwise = getFirstExprEvaluated xs n -- Else continue
 
 -- The end-to-end function
@@ -89,14 +90,56 @@ solution :: [Int] -> Int -> Maybe Expr
 solution [] n = Nothing
 solution x n = getFirstExprEvaluated (filterValid (constructAllExprFromArray x)) n -- Gets the first expression evaluated with the int given on all the expressions validated over all the expressions possible
 
+-------------
+-----Ex4-----
+-------------
 
+-- To clean mirrored Expression, we have to mark them as non-valid on the first path
+-- So we redefine the valid function
+validclean :: Op -> Int -> Int -> Bool
+validclean Add x y = x >= y -- (ie: a+b = b+a, but it will not filter a+a)
+validclean Sub x y = x > y -- same
+validclean Mul x y = x /= 1 && y /= 1 && x <= y -- (ie: 1*a = a, 1*b = b, a*b = b*a, but it does not filter a*a)
+validclean Div x y = y /= 1 && mod x y == 0 -- (ie: x/1 = x)
 
+-- Tells if an expression is valid recursivly
+isValidExpr' :: Expr -> Bool
+isValidExpr' (Val n) = True
+isValidExpr' (App op a b) = isValidExpr' a && isValidExpr' b && validclean op (eval a) (eval b) -- If the 2 Expressions are valid, we can test the validity on this one
 
+-- Filter on the only valid expressions
+filterValid' = filter isValidExpr'
 
+-- The end-to-end function elaged
+solution' :: [Int] -> Int -> Maybe Expr
+solution' [] n = Nothing
+solution' x n = getFirstExprEvaluated (filterValid' (constructAllExprFromArray x)) n
 
+-------------
+-----Ex5-----
+-------------
 
+-- Get an array of Expr already filtered on validity and binds the result
+getAllEval :: [Expr] -> [(Expr, Int)]
+getAllEval [] = []
+getAllEval (x:xs) = (x, eval x) : getAllEval xs
 
+findSolution :: [(Expr, Int)] -> Int -> Maybe (Expr, Int)
+findSolution [] _ = Nothing
+findSolution (x:xs) n 
+                   | snd x == n = Just (fst x, n)
+                   | otherwise = findSolution xs n
 
+-- int MUST be positive
+findSolutionRecursive :: [(Expr, Int)] -> Int -> (Expr, Int)
+findSolutionRecursive x (0) = ((Val 0),0) -- To avoid infinite loop 
+findSolutionRecursive x n = case findSolution x n of  
+                            Just a -> a
+                            Nothing -> findSolutionRecursive x (n-1)
+
+-- Given an array of Int and an Int, it gives the best solution with an Expr and the best Int close to the given one.
+bestSolution :: [Int] -> Int -> (Expr, Int)
+bestSolution x n = findSolutionRecursive (getAllEval (filterValid' (constructAllExprFromArray x))) n
 
 
 
